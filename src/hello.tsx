d97d9fd5568a5caf2c31cdbe9c11373d90feabad
JGluhov <jgluhov@gmail.com>
2016-12-31 12:52:55 +0300

    add onChange handler.

--- src/hello.tsx
+++ src/hello.tsx
@@ -5,9 +5,39 @@ type IHelloComponentProps = {
   value: number
 };
 
-type IHelloComponentState  = {};
+type IHelloComponentState  = {
+  message: string,
+  cat?: number
+};
 
 class HelloComponent extends React.Component<IHelloComponentProps, IHelloComponentState> {
+
+  constructor() {
+    super();
+
+    this.state = {
+      message: 'This is the state text',
+      cat: 7
+    };
+  }
+
+  onChangeHandler(e: Event) {
+    const inputElement = e.target as HTMLInputElement;
+
+    this.setState({ message: inputElement.value });
+  }
+
+  render() {
+    // const message = this.props.message!.replace(/New/g, 'Old');
+
+    return (
+      <div>
+        <input type='text' placeholder='Enter your message' onChange={this.onChangeHandler.bind(this)} />
+        <h1>Hello JGluhov - {this.state.message} - {this.props.value}</h1>
+        <div>Cat: {this.state.cat}</div>
+      </div>
+    );
+  }
 
   public static propTypes = {
     message: React.PropTypes.string,
@@ -17,16 +47,6 @@ class HelloComponent extends React.Component<IHelloComponentProps, IHelloCompone
   public static defaultProps = {
     message: 'Happy New Year'
   };
-
-  render() {
-    const message = this.props.message!.replace(/New/g, 'Old');
-
-    return (
-      <div>
-        <h1>Hello JGluhov - {message} - {this.props.value}</h1>
-      </div>
-    );
-  }
 }
 
 // const HelloComponent = () => <h1>Hello JGluhov</h1>;

c7659fbe4ef9ef2dd6c1e86686a41cb3b7b3d688
JGluhov <jgluhov@gmail.com>
2016-12-31 12:56:05 +0300

    renaming.

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import Greeting from './greeting';
-import Hello from './hello';
+import Greeting from './greeting.component';
+import Hello from './hello.component';
 
 ReactDOM.render(
   <div><Greeting name='JGluhov'/><Hello value={7}/></div>,
